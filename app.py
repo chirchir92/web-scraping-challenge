6af890a2f979052fd18997480f0082792026e00c
chirchir92 <russ.cheru@gmail.com>
2022-07-31 20:18:00 +1000

    code update

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask_pymongo import PyMongo
 import scrape_mars
 
 # creating instance for Flask
-app=Flask(__name__)
+# since i named my templates file i template, instead of renaming the file i\
+    # parsed in 'template_folder='
+app=Flask(__name__, template_folder='template')
 
 # use pymongo to establish mongo connection
 mongo=PyMongo(app, uri='mongodb://localhost:27017/mars_app')
@@ -12,20 +14,21 @@ mongo=PyMongo(app, uri='mongodb://localhost:27017/mars_app')
 # route to render index.html template using data from mongo
 @app.route('/')
 def home():
+
     # find all record of data from mongo database
-    mars_data=mongo.db.mars.find_one()
+    mars_data=mongo.db.collection.find_one()
     # return template and data
-    return render_template('index.html',mars=mars_data)
+    return render_template("index.html", mars=mars_data)
 
 # route to trigger scrape function
 @app.route('/scrape')
 def scrape():
 
     # run the scrape function
-    mars_info=scrape_mars.scrape()
+    mars_scrape=scrape_mars.scrape()
     
     # update the mongo db using update and upsert=True
-    mongo.db.mars.update({}, mars_info, upsert=True)
+    mongo.db.collection.update({}, mars_scrape, upsert=True)
     
     # redirect back to home page
     return redirect('/')
--- scrape_mars.py
+++ scrape_mars.py
@@ -4,8 +4,6 @@ from bs4 import BeautifulSoup as bs
 from splinter import Browser
 import pandas as pd
 import time
-import pymongo
-
 
 def scrape():
     # setup splinter
@@ -19,55 +17,75 @@ def scrape():
     image_url='https://marshemispheres.com/'
     browser.visit(url)
     
-    time.sleep()
     
     # use bs4 to parse the html
     html=browser.html
     soup=bs(html, 'html.parser')
     
+    #---------------------- MARS NEWS-----------------------------
     # perfome css selection on elements 'item-list & slide'
     # we use .select sourced from https://www.crummy.com/software/BeautifulSoup/bs4/doc/#css-selectors
     soup.select('ul.item_list li.slide')
+    
     # use .select_one to only return the first tag matching the selector
     # use a variable to enable use later
     css_selector=soup.select_one('ul.item_list li.slide')
+    
     # access the div class content_title to extract the heading
     css_selector.find('div', class_='content_title')
     news_title=css_selector.find('div', class_='content_title')
+    
     # access the class article teaser body to get the paragraoh text
     news_p=css_selector.find('div', class_='article_teaser_body')
     
-    # use .find_by function to locate the  image
-    image=browser.find_by_text('FULL IMAGE')
-    # confirm that the .find function has founf the picture
-    # to confirm use splinte .is_element_present function
-    confirm=browser.is_element_present_by_text('FULL IMAGE')
+
+    #-----------------------FEATURED IMAGE-------------------------------------
+    # # use .find_by_tag function to locate the  image
+    image=browser.find_by_tag('button')[1]
+    image.click()
     # inspect the html code to locate the image link and its div class
-    url_img=soup.find(class_='showimg fancybox-thumbs')['href']
-    featured=img_ulr.replace('index.html', url_img)
+    url_src=soup.find('img', class_='headerimage fade-in').get('src')
+    img_ulr=f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{url_src}'
     
+
+    #----------------------NARS FACTS------------------------------------
     fact=pd.read_html(url_facts)
     type(fact)
-    df=fact[1]
-    mars_facts=df.columns=['measurement','Mars profile']
+    df=fact[0]
+    mars_facts=df.columns=['measurement','Mars profile','Earth profile']
     
+
+    #--------------------HEMISPHERES-------------------------------------
+    hemisphere_image_urls=[]
+
     # identify the full resolution link 
     images=soup.find_all('div', class_='description')
-    title=image.find('h3')
+
+
+    for image in images:
+        image_dict={}
+
+        title=image.find('h3')
+        image_dict['title']=title.text
         
-    image_urls=soup.find('div', class_='item')
-    link=image_urls.find('img')['src']
+        image_url=soup.find('div', class_='item')
+        url_0=image_url.find('img')['src']
+        image_dict['img_url']=f'https://marshemispheres.com/{url_0}'
+        
+        hemisphere_image_urls.append(image_dict)
+        # print(f'hemisphere_image_urls={hemisphere_image_urls}')
     
+
+    #---------------------FINAL DICT---------------------------------
     # store data in a dictionary
     final_dict={
         'news_title': news_title,
         'news_p':news_p,
-        'featured':featured,
-        'nmars_facts':mars_facts,
+        'img_ulr':img_ulr,
+        'mars_facts':mars_facts,
         'title':title,
-        'image_urls':link
+        'hemisphere_image_urls':hemisphere_image_urls
     }
-
     browser.quit()
     
     # return results
